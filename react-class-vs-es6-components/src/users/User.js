dde2e29c3d1986c8a9d5f6ab08171a85b2937148
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-03-11 16:38:39 +0000

    passed user and using props

---
+++ react-class-vs-es6-components/src/users/User.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const User = (props) => {
+  return(
+    <div>
+      {/* User functional component test */}
+      {props.children}
+    </div>
+  )
+}
+
+export default User;
--- react-class-vs-es6-components/src/users/Users.js
+++ react-class-vs-es6-components/src/users/Users.js
@@ -1,10 +1,16 @@
 // class based component ... so we need to import component library
 import React, { Component } from 'react';
+import User from './User';
 
 export default class Users extends Component {
   render() {
     return(
-      <div>Class based component test</div>
+      // <div>Class based component test</div>
+      <div>
+        <User>AAA</User>
+        <User>BBB</User>
+        <User>CCC</User>
+      </div>
     )
   }
 
