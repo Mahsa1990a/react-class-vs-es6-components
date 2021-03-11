0e49b9b7c4e7b377974d563044c9e339d59a43a2
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-03-11 16:22:36 +0000

    added class based component

---
+++ react-class-vs-es6-components/src/users/Users.js
@@ -0,0 +1,11 @@
+// class based component ... so we need to import component library
+import React, { Component } from 'react';
+
+export default class Users extends Component {
+  render() {
+    return(
+      <div>Class based component test</div>
+    )
+  }
+
+}
